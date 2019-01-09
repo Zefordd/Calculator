b489f287ea081580d7caa84b4860b4f0da60765e
Сметанин Никита <n.a.smetanin@gmail.com>
2019-01-09 15:28:15 +0500

    подключена статика, добавлена шапка

--- calculator/views.py
+++ calculator/views.py
@@ -8,12 +8,11 @@ from aiohttp_session import get_session
 class Index(web.View):
     @aiohttp_jinja2.template('calculator/index.html')
     async def get(self):
-        conf = self.app['config']
         session = await get_session(self)
         user = ''
         if 'user' in session:
             user = session['user']
-        return dict(conf=conf, user=user)
+        return dict(user=user)
 
 class Feedback(web.View):
     @aiohttp_jinja2.template('calculator/feedback.html')
