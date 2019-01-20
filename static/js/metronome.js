b3935508ec43404c7065166f717914ce9af9962f
stounfo <stounfo@gmail.com>
2019-01-20 23:46:36 +0500

    сделан метроном с 4 долями

--- static/js/metronome.js
+++ static/js/metronome.js
@@ -3,9 +3,12 @@ var metronome = new Vue ({
     data: {
         start: false,
         beats_per_minute: 60,
+        audio: new Audio("../../static/tick.wav"), //так ток инвалиды делают, помогите
     },
     methods: {
         start_metronome: function () {
+            console.log(this.audio);
+            this.audio.play();
             console.log(0);
             i = 1;
             this.start = true;
@@ -13,6 +16,7 @@ var metronome = new Vue ({
         },
         do_one_beat: function () {
             console.log(i);
+            this.audio.play();
             if (this.start === false) {
                 clearInterval(interval);
             }
