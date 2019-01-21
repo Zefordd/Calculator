48d4dafb7b77544fdb1406fce15d5c3ae96896fe
stounfo <stounfo@gmail.com>
2019-01-21 18:30:41 +0500

    переделан метроном, сделан таймер для метронома

--- static/js/metronome.js
+++ static/js/metronome.js
@@ -4,23 +4,45 @@ var metronome = new Vue ({
         start: false,
         beats_per_minute: 60,
         audio: new Audio("../../static/tick.wav"), //так ток инвалиды делают, помогите
+
+        minutes: 10,
+        seconds: 15,
     },
     methods: {
         start_metronome: function () {
-            console.log(this.audio);
             this.audio.play();
             console.log(0);
             i = 1;
             this.start = true;
-            interval = setInterval(this.do_one_beat, 60000 / this.beats_per_minute);
+            this.do_one_beat();
         },
-        do_one_beat: function () {
-            console.log(i);
-            this.audio.play();
-            if (this.start === false) {
-                clearInterval(interval);
+        do_one_beat: async function () {
+            while (this.start === true) {
+                console.time('one_beat_time');
+                this.audio.play();
+                await this.sleep(60000 / this.beats_per_minute);
+                console.timeEnd('one_beat_time');
             }
-            i += 1;
+        },
+        sleep: function (ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        },
+
+        start_timer: async function () {
+            this.start = true;
+            this.do_one_beat();
+            while (this.minutes >= 0) {
+                while (this.seconds >= 1) {
+                    await this.sleep(1000);
+                    this.seconds -= 1;
+                }
+                if (this.minutes === 0) {
+                    break
+                }
+                this.minutes -= 1;
+                this.seconds = 60;
+            }
+            this.start = false;
         },
     },
     delimiters: ['[[',']]']
