d2307de6c33c71a988b6c9585f357f58949ccd42
Сметанин Никита <n.a.smetanin@gmail.com>
2019-01-29 13:52:27 +0500

    инкапсулирована функция make_spiral

--- calculator/views.py
+++ calculator/views.py
@@ -30,38 +30,39 @@ class Spiral(web.View):
     spiral = ''
     async def post(self):
         data = await self.post()
-        Spiral.spiral =  await make_spiral(data['dimension'])
+        Spiral.spiral =  await Spiral.make_spiral(data['dimension'])
         return web.HTTPFound(location=self.app.router['index'].url_for())
-
-
-
-
-async def make_spiral(n):
-    if n == '0' or n == '':
-        return 
-    n = int(n)
-    A = [[0 for i in range(n)]for j in range(n)]
-    k = 1
-    b = 0
-    i,j = 0,0
-    while A[i][j] <= n**2:
-        i += b
-        j += b
-        for j in range(b,n): #top
-            A[i][j] = k
-            k += 1
-        for i in range(b+1,n-1): #right
-            A[i][j] = k
-            k += 1
-        i += 1
-        for j in range(n-1,b,-1): #bot
-            A[i][j] = k
-            k += 1
-        j = b
-        for i in range(n-1,b,-1): #left
-            A[i][j] = k
-            k += 1
-        b += 1
-        n -= 1
+    
+    @staticmethod
+    async def make_spiral(n):
+        if n == '0' or n == '':
+            return 
+        n = int(n)
+        A = [[0 for i in range(n)]for j in range(n)]
+        k = 1
+        b = 0
         i,j = 0,0
-    return A
+        while A[i][j] <= n**2:
+            i += b
+            j += b
+            for j in range(b,n): #top
+                A[i][j] = k
+                k += 1
+            for i in range(b+1,n-1): #right
+                A[i][j] = k
+                k += 1
+            i += 1
+            for j in range(n-1,b,-1): #bot
+                A[i][j] = k
+                k += 1
+            j = b
+            for i in range(n-1,b,-1): #left
+                A[i][j] = k
+                k += 1
+            b += 1
+            n -= 1
+            i,j = 0,0
+        return A
+
+
+
